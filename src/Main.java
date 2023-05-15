967969eed2062280bd118b9de3fd17305d5ea8b8
Olya S <agloaveegres@gmail.com>
2023-05-16 01:24:01 +0300

    adding files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5000;
+        int amountForBonus = 20;
+        int miles = 1;
+        int result;
+        result = (price * miles) / amountForBonus;
+        System.out.println(result);
+    }
+}
